6cb808eb0eb68bd99ba75648a57a7fd654dccc9a
Edgar Khimich <edgarkhimich@gmail.com>
2018-10-25 08:31:04 -0600

    add es6 promise to mongoose

--- test/connection.js
+++ test/connection.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/testaroo')
+// Add ES6 promise to mongoose
+mongoose.Promise = global.Promise
 
-mongoose.connection.once('open', function () {
-    console.log('Connection have been made')
-}).on('error', function(error){
-    console.log('Connection error', error)
+// Connect to the database before test run
+before(function(done){
+    mongoose.connect('mongodb://localhost/testaroo', {useNewUrlParser: true})
+    mongoose.connection.once('open', function () {
+        console.log('Connection have been made')
+        done()
+    }).on('error', function(error){
+        console.log('Connection error', error)
+    })
 })
+
