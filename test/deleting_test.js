7e2ea39921db889f5367501800a700c56ebd76f4
Edgar Khimich <edgarkhimich@gmail.com>
2018-10-25 22:13:40 -0600

    deleting from mongodb

---
+++ test/deleting_test.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+const MarioChar = require('../models/mariochar')
+
+//Describes tests
+describe('Deleting records', function(){
+
+    var char
+
+    beforeEach(function(done){
+        char = new MarioChar({
+            name: 'Mario'
+        })
+
+        char.save().then(function(){
+            assert(char.isNew === false)
+            done()
+        })
+    })
+    
+    // Create tests
+
+    it('Delete one record from the database', function(done){
+
+        MarioChar.findOneAndRemove({name: 'Mario'}).then(function(){
+            MarioChar.findOne({name: 'Mario'}).then(function(result){
+                assert(result === null)
+                done()    
+            })
+        })
+    })
+})
